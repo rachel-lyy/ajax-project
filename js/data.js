731264241d83c9145f003ecfebd40cd8fb57d26a
Rachel Ly <rachel.ly17@gmail.com>
2023-04-19 20:40:19 +0000

    Filtered duplicate records and displayed results

--- js/data.js
+++ js/data.js
@@ -5,12 +5,14 @@ form.addEventListener('submit', function (e) {
   // create results
   function getJSON(data) {
     // take search terms and process it
+
     var formValue = document.querySelector('#search').value.split(' ').join('+');
     const xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://www.googleapis.com/books/v1/volumes?q=' + formValue + '&key=AIzaSyBi2qjgitOmNBWMLT-kqGh7yr4I84SZYkY');
     xhr.responseType = 'json';
     xhr.addEventListener('load', function () {
-
+      // console.log(xhr.status);
+      // console.log(xhr.response);
       var response = xhr.response.items;
       // remove duplicate results
       const outputArray = Object.values(
@@ -19,10 +21,21 @@ form.addEventListener('submit', function (e) {
 
       // loop through results and display on page
       for (let i = 0; i < 4; i++) {
-        var id = '#book' + i;
-        var item = document.querySelector(id);
         if (outputArray[i] !== undefined) {
-          item.textContent = outputArray[i].volumeInfo.title;
+          var id = '#book' + i;
+          var item = document.querySelector(id);
+
+          var newBookImg = document.createElement('img');
+          newBookImg.src = outputArray[i].volumeInfo.imageLinks.thumbnail;
+
+          var bookTitle = document.createElement('h3');
+          bookTitle.textContent = outputArray[i].volumeInfo.title;
+          var bookAuthor = document.createElement('h4');
+          bookAuthor.textContent = outputArray[i].volumeInfo.authors;
+          item.appendChild(newBookImg);
+          item.appendChild(bookTitle);
+          item.appendChild(bookAuthor);
+
         } else {
           return;
         }
